dfe3fa3a56bbaff5dae73f7fb517ba50139d1c60
sametemiroglu <sametemiroglu@hotmail.com>
2020-09-01 15:44:47 +0300

    v01

---
+++ dynamicalthreads.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import threading
+
+
+threads = []
+t_count = sys.argv[1]
+
+def proc(myThread):
+    print(myThread," : Started")
+
+for i in range(0,int(t_count)+1):
+    t = threading.Thread(target=proc,args=(i,))
+    t.daemon = True
+    threads.append(t)
+    t.start()
+
+for x,y in enumerate(threads):
+    y.join()
